943607ccad4f991eaf32fa7b4a8770082217c3a4
Ray Liu <wooshadyyy@gmail.com>
2022-09-25 14:27:25 -0400

    lab 1 第二次修改

---
+++ Lab1/test/Weather/WeatherReadingTest.java
@@ -0,0 +1,55 @@
+package Weather;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeatherReadingTest {
+  private WeatherReading weatherReading;
+  @Before
+  public void setUp() throws Exception {
+    weatherReading = new WeatherReading(23, 12, 3, 12);
+
+  }
+
+  @Test
+  public void getTemperature() {
+    System.out.println(weatherReading.getTemperature());
+  }
+
+  @Test
+  public void getDewPoint() {
+    System.out.println(weatherReading.getDewPoint());
+  }
+
+  @Test
+  public void getWindSpeed() {
+    System.out.println(weatherReading.getWindSpeed());
+  }
+
+  @Test
+  public void getTotalRain() {
+    System.out.println(weatherReading.getTotalRain());
+  }
+
+  @Test
+  public void getRelativeHumidity() {
+    System.out.println(weatherReading.getRelativeHumidity());
+  }
+
+  @Test
+  public void getHeatIndex() {
+    System.out.println(weatherReading.getHeatIndex());
+  }
+
+  @Test
+  public void getWindChill() {
+    System.out.println(weatherReading.getWindChill());
+  }
+
+  @Test
+  public void testToString() {
+    System.out.println(weatherReading);
+  }
+}
